3e5870bf14abc2728943d9770c6984231cc802da
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-10 15:43:14 +0530

    Refactor app structure by removing unused components and integrating customer input in Home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,29 +4,14 @@ import { Nav } from "./page/nav/nav";
 import { Footer } from "./page/footer/footer";
 import { FormsModule } from '@angular/forms';
 
-interface Customer{
-  name:string;
-  age:number;
-  address:string;
-}
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, Nav, Footer],
+  imports: [FormsModule, Nav, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   
-  customer :Customer={
-    name:'',
-    age:0,
-    address:'',
-  }
-
-  name:string="";
-
-  Clickme(){
-    alert("hii yaluwe")
-  }
 }
--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+interface Customer{
+  name:string;
+  age:number;
+  address:string;
+}
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  customer :Customer={
+    name:'',
+    age:0,
+    address:'',
+  }
+
+  name:string="";
+
+  Clickme(){
+    alert("hii yaluwe")
+  }
 
 }
