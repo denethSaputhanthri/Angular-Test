af25bb9f331a1bbfc9c5fbd85101fd2787029adb
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-10 15:22:00 +0530

    Add routing for About, Contact, and Home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
+import { About } from './page/about/about';
+import { Contact } from './page/contact/contact';
+import { Home } from './page/home/home';
 
 export const routes: Routes = [
     {
         path:"about",
+        component:About,
     
     },
+    {
+        path:"contact",
+        component:Contact,
+    
+    },
+    {
+        path:"home",
+        component:Home,
+    
+    },
+
 ];
