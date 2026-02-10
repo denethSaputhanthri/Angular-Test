690493faacbb20be8a4bc1abf6ae80f60f9d41a7
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-10 15:01:06 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./page/nav/nav";
+import { Footer } from "./page/footer/footer";
+import { FormsModule } from '@angular/forms';
+
+interface Customer{
+  name:string;
+  age:number;
+  address:string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('demo-app');
+  
+  customer :Customer={
+    name:'',
+    age:0,
+    address:'',
+  }
+
+  name:string="";
+
+  Clickme(){
+    alert("hii yaluwe")
+  }
 }
